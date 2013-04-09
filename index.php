eb8c1581d35afbe5c22cb23205afad7783025bb7
David <david.roth@aspirepublicschools.org>
2013-04-09 13:12:25 -0700

    test

--- index.php
+++ index.php
@@ -46,7 +46,7 @@ $app_using_friends = $facebook->api(array(
     'method' => 'fql.query',
     'query' => 'SELECT name, education FROM user WHERE uid=me()'
  ));
- print_r($app_using_friends);
+ print_r($app_using_friends[0]['education']);
   	//$testarray = $app_using_friends[0]['education'];
    //echo "Test array";
    //print_r($testarray);
